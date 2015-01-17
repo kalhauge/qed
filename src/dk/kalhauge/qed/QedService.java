6710957c11048376f2f9fb665a1737613f5e6f58
Christian Kalhauge <christian@kalhauge.dk>
2015-01-17 15:57:13 +0100

    Cleaned QedService

--- src/dk/kalhauge/qed/QedService.java
+++ src/dk/kalhauge/qed/QedService.java
@@ -6,31 +6,65 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
+import java.util.logging.Logger;
 
+/** The QedService setup a quick HttpServer. To start one
+ * quickly use the command:
+ *
+ *      QedService.fromFacade(facade).start().readCommandsAndStop();
+ *
+ */
 public class QedService {
+  private static final Logger log = Logger.getLogger(QedService.class.getName());
+
   private static final int PORT = 4711;
   private final File root;
+  private final Object facade;
   private HttpServer server;
 
-  public QedService(Object root, Object facade) throws IOException {
-    if (root instanceof File) this.root = (File)root;
-    else this.root = new File(root.getClass().getResource("/").getPath());
-    this.server = HttpServer.create(new InetSocketAddress("localhost", PORT), 0);
-    server.createContext("/", new FileHandler(this.root));
+  public QedService(File root, Object facade) {
+    this.root = root;
+    this.facade = facade;
+    }
+
+  public static QedService fromFacade(Object facade) {
+      return new QedService(
+        new File(facade.getClass().getResource("/").getPath()),
+        facade
+        );
+    }
+
+  public QedService stop() {
+    server.stop(0);
+    this.server = null;
+    log.fine("Stopped QedService");
+    return this;
+    }
+
+  public QedService start(InetSocketAddress addr) {
+    log.fine("Starting QedService");
+    server = HttpServer.create(addr, 0);
+    HttpServer server = HttpServer.create(addr, 0);
+    server.createContext("/", new FileHandler(root));
     server.createContext("/qed", new QedHandler(facade));
-    System.out.println("QED Service listening on "+PORT);
     server.start();
+    log.fine("Started QedService");
+    return this;
+    }
+
+  public QedService start() throws IOException {
+    return start(new InetSocketAddress("localhost", PORT));
+    }
+
+  public QedService readCommandsAndStop() {
     BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     String command = in.readLine();
     while (command != null) {
       if("exit".equals(command)) break;
       command = in.readLine();
       }
-    server.stop(0);
+    return stop();
     }
   
-  public void stop() {
-    server.stop(0);
-    }
   
   }
